3844d3e427cbb1750b0a69d70c6d8e5cbed995b3
JUCIEN <117264468+SenpaiMC@users.noreply.github.com>
2025-05-14 01:52:02 +0400

    fichier: 127_0_0_1(2).sql

--- sql/127_0_0_1(2).sql
+++ sql/127_0_0_1(2).sql
@@ -10,22 +10,11 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de données : `sakura_scan`
 --
 CREATE DATABASE IF NOT EXISTS `sakura_scan` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `sakura_scan`;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `chapitres`
 --
 
